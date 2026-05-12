009ddd72b67fde74dbc3d20fbef09941842f0724
pnxaccess-creator <pnxaccess@gmail.com>
2026-05-12 22:14:34 +0530

    fix astro deployment

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,18 +1,16 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  site: 'https://dev.ecom',
+  output: 'server',
 
   adapter: vercel(),
-
-  output: 'server',
 
   vite: {
     resolve: {
